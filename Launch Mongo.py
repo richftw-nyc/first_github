52f1343d0e6a4b0b0f9517322f65bfa93cef36be
richftw-nyc <hy990@stern.nyu.edu>
2018-05-27 16:36:41 -0400

    creating first file

---
+++ Launch Mongo.py
@@ -0,0 +1,19 @@
+import subprocess, os, shlex
+
+class Mongo_Manager:
+    def __init__(self, data_path):
+        self.data_path = data_path
+
+    def launch_mongodb(self, mongo_path):
+        self.mongod = subprocess.Popen(shlex.split(mongo_path + 'mongod' +
+                                                   " --dbpath {0}".format(os.path.expanduser(self.data_path))))
+        self.test_connection = subprocess.Popen(shlex.split(mongo_path + 'mongo'))
+        return self.mongod, self.test_connection
+
+
+if __name__ == '__main__':
+    path1 = '/Users/richardyang/Desktop/Research/mongodb/data/'
+    path2 = '/Users/richardyang/Desktop/Research/mongodb/bin/'
+
+    my_manager = Mongo_Manager(path1)
+    my_manager.launch_mongodb(path2)
